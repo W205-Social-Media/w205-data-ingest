5ac37e8b0a86677c797f2733c1ff9e19b3e1618e
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-13 14:06:26 -0600

    venue

--- stream.py
+++ stream.py
@@ -5,7 +5,7 @@ import json
 sc = SparkContext("local[2]", "MyApp")
 ssc = StreamingContext(sc, 10)
 lines = ssc.socketTextStream("localhost", 9999)
-jslines = lines.flatMap(lambda x: [ j['tweet'] for j in json.loads('['+x+']') if 'tweet' in j] )
+jslines = lines.flatMap(lambda x: [ j['venue'] for j in json.loads('['+x+']') if 'venue' in j] )
 lcnt=lines.count()
 lcnt.pprint()
 c=jslines.count()
