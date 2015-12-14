81248a700845071567baee839b8775db996f7117
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-14 09:34:48 -0600

    log

--- testrun.py
+++ testrun.py
@@ -69,9 +69,9 @@ class FacebookDataIngestSource:
 
 
 
-    
+    self.log('%s: %s -> %s' % (self.page_index,page_name,video_json))      
     return self
-  self.log('%s: %s -> %s' % (self.page_index,page_name,video_json))      
+    
 
 term = str(sys.argv)
 
